3545da1805257c2cf823a0a68f29743c284fba5d
emmanuel nnaji <realreach@yahoo.com>
2023-12-04 05:23:04 +0000

    create shell scripts that run python scripts

---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,2 @@
+This project taks forms our formal introduction into PYTHON programminig
+And these are the different project tasks:
---
+++ 0x00-python-hello_world/example.py
@@ -0,0 +1,13 @@
+def calculate_sum(a, b):
+    return a + b
+
+
+def main():
+    x = 5
+    y = 10
+    total = calculate_sum(x, y)
+    print(f"the sum of {x} and {y} is {total}.")
+
+
+if __name__ == '__main__':
+    main()
