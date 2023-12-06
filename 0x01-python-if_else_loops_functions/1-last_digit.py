6a7f8c6f6449ece592c7e6f895c086dfdc1c90d5
emmanuel nnaji <realreach@yahoo.com>
2023-12-06 12:11:49 +0000

    prints only required lower case alphabets

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,12 +5,18 @@ abs_number = number
 if abs_number < 0:
     abs_number *= -1
 
+# to get last digit
 last_digit = abs_number % 10
 
+# to include negative sign
+if number < 0 and last_digit != 0:
+    last_digit *= -1
+
+# print outcome
 if last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit < 6 and last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less \
-            than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less "
+          f"than 6 and not 0")
 elif last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
