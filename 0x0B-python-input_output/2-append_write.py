0e3df77615b98b7edb8261c933a6e2f963bf564c
emmanuel nnaji <realreach@yahoo.com>
2024-01-11 01:03:50 +0000

    fxn appends text to given file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 
-def write_file(filename="", text=""):
-    '''fxn writes a string to file
+def append_write(filename="", text=""):
+    '''fxn appends a string to file,
         create the file if not exist.
     Args:
-        fileneame (obj): the given file
-        text (string): the string to be written
-    Return:
-        the number of characters written
+        fileneame (str): the given file name
+        text (str): the string to be written
+    Return (int): the number of characters written
     '''
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'a', encoding="utf-8") as f:
         num = f.write(text)
         return num
