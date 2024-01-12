1e44ee8a14a8f95516769cbbb860580c3d3bafe5
emmanuel nnaji <realreach@yahoo.com>
2024-01-12 12:59:05 +0000

    fxn imports calculator modules and perform calculations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    # import modules
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+
+    # validate input
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    # declare values
+    operator = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
+
+    # perform operations
+    match operator:
+        case '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        case '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        case '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        case '/':
+            print("{} / {} = {}".format(a, b, div(a, b)))
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -5,3 +5,4 @@ Thesenare the different tasks solved:
 0-add.py - fxn import a module to add two numbers
 1-calculation.py - fxn print CLI argument with index
 3-infinite_add.py - script add infinit number
+5-variable_load.py - script import a variable and print it
