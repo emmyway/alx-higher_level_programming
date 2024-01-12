8dc9ea68b00f0075bc7c325ccd6334fbffe03d2b
emmanuel nnaji <realreach@yahoo.com>
2024-01-12 09:53:01 +0000

    fxn print CLI argument with index

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    print("{} arguments".format(len(argv) - 1))
+    for i, args in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, args))
