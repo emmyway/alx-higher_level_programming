27647cf03f55759c70bcb01fc9d283db0e4ee472
emmanuel nnaji <realreach@yahoo.com>
2024-01-12 11:13:37 +0000

    made corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,12 @@
 if __name__ == "__main__":
     from sys import argv
 
-    print("{} arguments".format(len(argv) - 1))
+    if (len(argv) >= 2):
+        s = "s"
+        col = ":"
+    else:
+        s = ""
+        col = "."
+    print("{} argument{}{}".format(len(argv) - 1, s, col))
     for i, args in enumerate(argv[1:], start=1):
         print("{}: {}".format(i, args))
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -2,3 +2,5 @@ This is an alx course student project, designed to deepen our knowledge of impor
 
 Thesenare the different tasks solved:
 
+0-add.py - fxn import a module to add two numbers
+1-calculation.py - fxn print CLI argument with index
