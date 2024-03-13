76d7c51a451190b8292b0517badcf47b4b08bc14
emmanuel nnaji <realreach@yahoo.com>
2024-03-13 11:35:01 +0100

    added script that draws a square with X for CL size passed

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const myArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+while (myArr[i]) {
+  console.log(myArr[i]);
+  i++;
+}
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const xLen = parseInt(process.argv[2]);
+
+if (isNaN(xLen)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < xLen; i++) {
+    let row = '';
+    for (let j = 0; j < xLen; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
