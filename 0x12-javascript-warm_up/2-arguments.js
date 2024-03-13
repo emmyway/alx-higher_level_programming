3075137486711957971b7eab339b1397edfa481a
emmanuel nnaji <realreach@yahoo.com>
2024-03-13 09:03:38 +0100

    added a java script basic that prints to console

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// cal. argument length
+let myArrLen = process.argv.length;
+//compare and infer
+if (myArrLen === 2)
+	console.log('No argument');
+else if (myArrLen === 3)
+	console.log('Argument found');
+else
+	console.log('Arguments found');
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,1 @@
+0-javascript_is_amazing.js 1-multi_languages.js 3-value_argument.js 4-concat.js
