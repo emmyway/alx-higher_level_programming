11913631cd324b347e1cdb30816efdc54eaf0435
emmanuel nnaji <realreach@yahoo.com>
2024-03-13 22:05:29 +0100

    added scripts that finds factorial of number, uses require() to affect another script and debugged task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+// prints result os sum of two command line int passed
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+function add (a, b) {
+  return (a + b);
+}
+
+console.log(add(a, b));
