1f9b909292cac060bab18d016c8a3b531b34f303
emmanuel nnaji <realreach@yahoo.com>
2024-04-15 04:15:35 +0100

    debugged to meet requirment

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,24 +4,28 @@ import sys
 '''
 python script to manipulate a database table
 '''
+
+
 def list_states(username, password, database):
     '''
     script lists all rows in table (state)
     '''
     # connect to mysql database
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
     # creating the cursor object
     cur = db.cursor()
     # selecting all states row
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     # fetching all
-    output  = cur.fetchall()
+    output = cur.fetchall()
     # printing states
     for state in output:
         print(state)
     # cleaning up
     cur.close()
     db.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -32,5 +36,3 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     list_states(username, password, database)
-
-
