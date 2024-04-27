16313489dd4dc300cd9b3629c4fccbd15789e3d2
emmanuel nnaji <realreach@yahoo.com>
2024-04-27 22:52:44 +0100

    added task 2-4

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
-'''\nscript'''
+'''
+script takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+'''
 import urllib.request
+import sys
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        # send s the request
+        with urllib.request.urlopen(url) as response:
+            body = response.read().decode("utf-8")
+
+        print(body)
+
+        # catches error
+    except urllib.error.HTTPError as e:
+        print("Error code", e.code)
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
-'''\nscript'''
-import urllib.request
+'''
+script fetches https://alx-intranet.hbtn.io/status using
+request
+'''
+import requests
+
+if __name__ == "__main__":
+    url = https://alx-intranet.hbtn.io/status
+    
+    # send request
+    response = requests.get(url)
+
+    # display output
+    print("Body response")
+    print("\t- type:", type(response.text))
+    print("\t- content:", respons4.text)
