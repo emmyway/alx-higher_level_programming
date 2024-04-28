2702f9c4f7be85aa283a95fee8bfeb5c49234ba6
emmanuel nnaji <realreach@yahoo.com>
2024-04-28 09:48:42 +0100

    added solution to task 5-7

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-'''\nscript'''
-import urllib.request
+'''
+script
+'''
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    response = requests.post(url, data=value)
+
+    print(response.text)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
-'''\nscript'''
-import urllib.request
+'''
+script takes in a URL, sends a request to the URL and displays
+the body of the response, with http status code using requests
+'''
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    
+    if response.status_code >= 400:
+        print("Error code:" response.status_code)
+        sys.exit()
+    print(response.text)
