883cc360c642a3b249b9d981c7b6584b94d21b48
emmanuel nnaji <realreach@yahoo.com>
2024-05-22 05:09:53 +0100

    added fuction that writes to file with promise

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// import the fs primises
+const fs = require('fs').promises;
+// write file in try-cache function
+async function writeFileAsync(filePath, content) {
+	try {
+		await fs.writeFile(filePath, content, 'utf-8');
+	} catch (error) {
+		console.error(error);
+	}
+}
+// call function with arguments
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+if (filePath && content) {
+	writeFileAsync(filePath, content);
+} else {
+	console.error("please privide file path and string to wtite");
+}
