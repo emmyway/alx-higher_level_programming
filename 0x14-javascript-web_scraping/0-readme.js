877b7960542713a52d4f3c7167fe443132b088f2
emmanuel nnaji <realreach@yahoo.com>
2024-05-22 05:59:32 +0100

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,22 +4,22 @@
 const fs = require('fs').promises;
 
 // defining an async function
-async function readFileAsync(filePath) {
-	try {
-		//use fs.readFile to read file, await to wait 
-		// for the promise to complete reading file
-		const data = await fs.readFile(filePath, 'utf-8');
-		console.log(data);
-	} catch (error) {
-		console.error(error)
-	}
+async function readFileAsync (filePath) {
+  try {
+    // use fs.readFile to read file, await to wait
+    // for the promise to complete reading file
+    const data = await fs.readFile(filePath, 'utf-8');
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // get the file path from first command line
 const filePath = process.argv[2];
 
 if (filePath) {
-	readFileAsync(filePath);
+  readFileAsync(filePath);
 } else {
-	console.error("pls provide a file path");
+  console.error('pls provide a file path');
 }
