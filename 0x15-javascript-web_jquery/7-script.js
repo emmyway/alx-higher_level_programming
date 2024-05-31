a5fff836353b7aec86a1ee4465fa8aefcfca2bf3
Emmanuel Nnaji <realreaach@yahoo.com>
2024-05-31 09:15:35 +0100

    update task 7

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-	const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
-	// make request
-	$.getJSON(url, function(data) {
-		const characterName = data.name;
+  const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+  // make request
+  $.getJSON(url, function (data) {
+    const characterName = data.name;
 
-		// display the name in the DIV element
-		$('DIV#character').text(characterName);
-	});
+    // display the name in the DIV element
+    $('DIV#character').text(characterName);
+  });
 });
