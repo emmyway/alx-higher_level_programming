46f7c6401af1638178bfc0aebb49ae9fe72cc9c1
emmanuel nnaji <realreach@yahoo.com>
2024-07-22 21:12:04 +0100

    added the catch me file and the peak files

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' script finds the peak in an array
+'''
+
+
+def find_peak(list_of_integers):
+    '''
+    finds a peak in a list of unsorted integers
+    Args:
+        list_of_integers (lidt): A list of unsorted integers
+    Returns:
+        int: the peak element jn the list.
+    '''
+    # validate list
+    if not list_of_integers
+
